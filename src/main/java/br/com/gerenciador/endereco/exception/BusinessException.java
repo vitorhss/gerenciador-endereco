8c4a8cdfb6c6fcef0bb5a278328e32e6b43557ed
Vitor Souza <vitor.souza@b2wdigital.com>
2017-04-18 17:42:22 -0300

    BusinessException com construtor recebendo apenas a mensagem

--- src/main/java/br/com/gerenciador/endereco/exception/BusinessException.java
+++ src/main/java/br/com/gerenciador/endereco/exception/BusinessException.java
@@ -11,5 +11,9 @@ public class BusinessException extends RuntimeException {
 	public BusinessException(String message, Throwable cause) {
 		super(message, cause);
 	}
+	
+	public BusinessException(String message){
+		super(message);
+	}
 
 }
