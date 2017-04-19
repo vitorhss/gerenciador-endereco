88948087f60fa7049f5dff46f48bb0d40ac00ce9
Vitor Souza <vitor.souza@b2wdigital.com>
2017-04-19 18:06:05 -0300

    Listagem de endereços. Serviço

--- src/main/java/br/com/gerenciador/endereco/service/EnderecoService.java
+++ src/main/java/br/com/gerenciador/endereco/service/EnderecoService.java
@@ -1,7 +1,10 @@
 package br.com.gerenciador.endereco.service;
 
+import java.util.List;
+
 import br.com.gerenciador.endereco.domain.Endereco;
 
 public interface EnderecoService {
 	Endereco getEndereco(String cep);
+	List<Endereco> listarEnderecos();
 }
--- src/main/java/br/com/gerenciador/endereco/service/impl/EnderecoServiceImpl.java
+++ src/main/java/br/com/gerenciador/endereco/service/impl/EnderecoServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.gerenciador.endereco.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,7 @@ import br.com.gerenciador.endereco.domain.Endereco;
 import br.com.gerenciador.endereco.exception.BusinessException;
 import br.com.gerenciador.endereco.repository.EnderecoRepository;
 import br.com.gerenciador.endereco.service.EnderecoService;
+import br.com.gerenciador.endereco.utils.Utils;
 
 @Service
 public class EnderecoServiceImpl implements EnderecoService {
@@ -15,16 +18,20 @@ public class EnderecoServiceImpl implements EnderecoService {
 
 	@Override
 	public Endereco getEndereco(String cep) {
-		if(!isValidCep(cep)){
+		Endereco endereco = new Endereco();
+
+		if (!Utils.isValidCep(cep)) {
 			throw new BusinessException("O cep informado não é válido");
 		}
-		Endereco endereco = enderecoRepository.findByCep(cep); 
-		
+
+		endereco = enderecoRepository.findByCep(cep);
+
 		return endereco;
 	}
-	
-	private boolean isValidCep(String cep){
-		return true;
+
+	@Override
+	public List<Endereco> listarEnderecos() {
+		return enderecoRepository.findAll();
 	}
 
 }
