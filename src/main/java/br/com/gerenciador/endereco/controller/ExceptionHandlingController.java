329a69e1edd8a63dff5bba31b1ceb3b40331b767
Vitor Souza <vitor.souza@b2wdigital.com>
2017-04-21 01:37:02 -0300

    Atualizando retorno de busines exception

--- src/main/java/br/com/gerenciador/endereco/controller/ExceptionHandlingController.java
+++ src/main/java/br/com/gerenciador/endereco/controller/ExceptionHandlingController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import br.com.gerenciador.endereco.domain.FieldErro;
 import br.com.gerenciador.endereco.exception.BusinessException;
 import br.com.gerenciador.endereco.exception.ValidationException;
+import br.com.gerenciador.endereco.domain.Error;
 
 @ControllerAdvice
 public class ExceptionHandlingController {
